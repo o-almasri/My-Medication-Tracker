f48dfc6aa4a2e48f1ab7affa011af39827e807bc
Omar Almasri <o_almasri@fanshaweonline.ca>
2023-02-20 18:10:09 -0500

    [#13] notification work but missing a calender for scheduling future notifications

--- MyMedicationTracker/app/src/main/java/com/example/mymedicationtracker/MainActivity.kt
+++ MyMedicationTracker/app/src/main/java/com/example/mymedicationtracker/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.mymedicationtracker
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.Gson
@@ -22,7 +27,8 @@ class MainActivity : AppCompatActivity() , View.OnClickListener{
     lateinit var list:ArrayList<entry>
 
 
-
+    private val CHANNEL_ID = "channel_id_example_01"
+    private val notificationId = 101
 
 
 
@@ -31,7 +37,7 @@ class MainActivity : AppCompatActivity() , View.OnClickListener{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        createNotificationChannel()
 
         addbtn = findViewById(R.id.addbtn);
         addbtn.setOnClickListener(this);
@@ -81,7 +87,10 @@ class MainActivity : AppCompatActivity() , View.OnClickListener{
 
 
 
-
+        val btn = findViewById<Button>(R.id.bbtn)
+        btn.setOnClickListener{
+            sendNotification()
+        }
 
 
 
@@ -129,6 +138,32 @@ class MainActivity : AppCompatActivity() , View.OnClickListener{
        // Toast.makeText(this, " Paused", Toast.LENGTH_SHORT).show();
 
     }
+
+    private fun createNotificationChannel(){
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.O){
+            val name = "Medication Time"
+            val descriptionText = "Please take the Following Medication"
+            val importance = NotificationManager.IMPORTANCE_HIGH;
+            val channel = NotificationChannel(CHANNEL_ID,name,importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE)as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun sendNotification(){
+        val builder = NotificationCompat.Builder(this,CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Medication Time")
+            .setContentText("Please take the Following Medication")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+        with(NotificationManagerCompat.from(this)){
+            notify(notificationId,builder.build())
+        }
+    }
+
+
 
     override fun onClick(btn: View?) {
 
