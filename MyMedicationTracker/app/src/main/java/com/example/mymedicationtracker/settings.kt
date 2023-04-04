c3d86b8fc75eeaa4a3407700c0f1c37ce936ce73
Omar Almasri <o_almasri@fanshaweonline.ca>
2023-04-04 07:29:59 -0400

    [#11] adding layout

---
+++ MyMedicationTracker/app/src/main/java/com/example/mymedicationtracker/settings.kt
@@ -0,0 +1,13 @@
+package com.example.mymedicationtracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class settings : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+
+    }
+}
